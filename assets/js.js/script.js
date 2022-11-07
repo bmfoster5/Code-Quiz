bf82b3239f1bdca6525d313aee92314ae87b01d8
bmfoster5 <foster.brandon96@gmail.com>
2022-11-07 10:44:44 -0500

    askBCS commit

---
+++ assets/js.js/script.js
@@ -0,0 +1,17 @@
+// Create variables for functionality
+var
+var
+var
+var
+
+
+// Create function to begin Quiz
+
+
+// Create questions and answers
+
+
+// Create if/else statements for correct and incorrect answers
+
+
+
